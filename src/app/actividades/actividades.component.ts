ba7aad2f92549bff298a8261393a72f0de5bab60
Rodrigo Pesenti <RodrigoPesenti1@hotmail.com>
2023-10-11 19:28:59 -0300

    .

--- src/app/actividades/actividades.component.ts
+++ src/app/actividades/actividades.component.ts
@@ -23,7 +23,7 @@ export class ActividadesComponent implements OnInit {
     if (principalDiv) {
       this.actividades.forEach(actividad => {
         const cardDiv = document.createElement('div');
-        cardDiv.classList.add('card', 'p-4', 'mx-2');
+        cardDiv.classList.add('card', 'p-1', 'mx-2');
         cardDiv.style.width = '18rem';
 
         const img = document.createElement('img');
@@ -32,22 +32,18 @@ export class ActividadesComponent implements OnInit {
         img.alt = 'Imagen de ' + actividad;
         img.style.objectFit = 'contain';
         img.style.maxWidth = '100%'; 
-        img.style.height = 'auto';  
+        img.style.height = '250px';  
 
         const cardBodyDiv = document.createElement('div');
         cardBodyDiv.classList.add('card-body', 'justify-content-center', 'align-items-center');
 
-        const cardTitle = document.createElement('h5');
+        const cardTitle = document.createElement('h1');
         cardTitle.classList.add('card-title', 'text-center');
         cardTitle.textContent = actividad.nombre;
 
-        const cardLink = document.createElement('a');
-        cardLink.href = '#';
-        cardLink.classList.add('btn', 'btn-info', 'd-flex', 'justify-content-center', 'align-items-center');
-        cardLink.textContent = 'Ir a ' + actividad.nombre;
+        
 
         cardBodyDiv.appendChild(cardTitle);
-        cardBodyDiv.appendChild(cardLink);
 
         cardDiv.appendChild(img);
         cardDiv.appendChild(cardBodyDiv);
