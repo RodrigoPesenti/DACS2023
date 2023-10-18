e4788d8a8def966feee085682fc2f4ee6770a5dd
rodrigopesenti55 <rodrigopesenti55@gmail.com>
2023-10-18 18:58:51 -0300

    .

--- Front/src/app/actividades/actividades.component.ts
+++ Front/src/app/actividades/actividades.component.ts
@@ -8,9 +8,12 @@ import { Component, OnInit } from '@angular/core';
 export class ActividadesComponent implements OnInit {
 
   actividades = [
-      {nombre: 'Senderismo', imagen: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSRfFIHA0tmqGr2xIjnvFdIU8htfx0UzsMctg&usqp=CAU' } ,
-      {nombre: 'Ciclismo', imagen: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSRfFIHA0tmqGr2xIjnvFdIU8htfx0UzsMctg&usqp=CAU' }, 
-      {nombre: 'Correr', imagen: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSRfFIHA0tmqGr2xIjnvFdIU8htfx0UzsMctg&usqp=CAU' } ,
+      {nombre: 'Pesca', imagen: 'https://media-public.canva.com/89OdA/MAD95E89OdA/1/tl.png' } ,
+      {nombre: 'Senderismo', imagen: 'https://media-public.canva.com/mQYyw/MAEWSDmQYyw/1/tl.png' }, 
+      {nombre: 'Correr', imagen: 'https://media-public.canva.com/nhCaI/MAEcNYnhCaI/1/tl.png' } ,
+      {nombre: 'Golf', imagen: 'https://media-public.canva.com/17QDU/MAEpaH17QDU/1/tl.png'},
+      {nombre: 'Camping', imagen: 'https://media-public.canva.com/l3pQo/MAEZBml3pQo/1/tl.png'},
+      {nombre: 'Football', imagen: 'https://media-public.canva.com/IpIqA/MAEiSZIpIqA/1/tl.png'}
     ];
 
   ngOnInit(): void {
@@ -24,23 +27,26 @@ export class ActividadesComponent implements OnInit {
       this.actividades.forEach(actividad => {
         const cardDiv = document.createElement('div');
         cardDiv.classList.add('card', 'p-1', 'mx-2');
-        cardDiv.style.width = '18rem';
+        cardDiv.style.width = '18vw';
+        cardDiv.style.height = '18vw';
 
         const img = document.createElement('img');
-        img.src = actividad.imagen; 
-        img.classList.add('card-img-top');
+        img.src = actividad.imagen;
+        img.classList.add('card-img-top', "mt-4");
         img.alt = 'Imagen de ' + actividad;
         img.style.objectFit = 'contain';
-        img.style.maxWidth = '100%'; 
-        img.style.height = '100%';  
+        img.style.maxWidth = '10vw';
+        img.style.height = '10vw';
+        img.style.display = 'block';
+        img.style.margin = 'auto';
 
         const cardBodyDiv = document.createElement('div');
-        cardBodyDiv.classList.add('card-body', 'justify-content-center', 'align-items-center');
+        cardBodyDiv.classList.add('card-body');
 
         const cardTitle = document.createElement('h5');
         cardTitle.classList.add('card-title', 'text-center', 'mb-0');
         cardTitle.textContent = actividad.nombre;
-        cardTitle.style.fontSize = '3vw';
+        cardTitle.style.fontSize = '2vw';
         
 
         cardBodyDiv.appendChild(cardTitle);
--- Front/src/app/mapa/mapa.component.ts
+++ Front/src/app/mapa/mapa.component.ts
@@ -1,12 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-mapa',
   templateUrl: './mapa.component.html',
   styleUrls: ['./mapa.component.css']
 })
-export class MapaComponent {
+export class MapaComponent implements OnInit {
+
+  public busqueda = ""
 
   constructor(){}
 
+  public async ngOnInit() {
+    // Selecciona el elemento iframe por su etiqueta
+    var iframe = document.querySelector('iframe');
+
+    if (iframe) {
+      // Obtiene el valor actual del atributo src
+      var srcActual = iframe.getAttribute('src');
+
+      // Concatena la variable al final del valor actual del atributo src
+      var nuevoSrc = srcActual + "Hotel";
+
+      // Asigna el nuevo valor del atributo src al iframe
+      iframe.setAttribute('src', nuevoSrc);
+    }
+  }
 }
