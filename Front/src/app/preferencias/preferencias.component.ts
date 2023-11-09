1262d2f868f9940b2c6f2c788cbadbb756a650ee
Marvin <marvin17lv@gmail.com>
2023-11-09 15:46:49 -0300

    .

--- Front/src/app/preferencias/preferencias.component.ts
+++ Front/src/app/preferencias/preferencias.component.ts
@@ -1,5 +1,8 @@
+import { ApiService } from './../core/services/apiservice.service';
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { KeycloakService } from 'keycloak-angular';
+import { KeycloakProfile } from 'keycloak-js';
 
 @Component({
   selector: 'app-preferencias',
@@ -8,12 +11,44 @@ import { FormsModule } from '@angular/forms';
 })
 export class PreferenciasComponent {
   pesca=false
+  public perfilUsuario: KeycloakProfile | null = null;
+  public isLogueado = false;
+  activities: { name: string, selected: boolean }[] = [];
 
-  activities = [
-    { name: 'Correr', selected: false },
-    { name: 'Pesca', selected: false },
-    { name: 'Senderismo', selected: false }
-  ];
+  constructor(private readonly keycloak: KeycloakService,private apiService: ApiService) {}
+
+  async ngOnInit(){
+    this.isLogueado = await this.keycloak.isLoggedIn();
+
+    if (this.isLogueado) {
+      this.perfilUsuario = await this.keycloak.loadUserProfile();
+      let nombreUsuario = <string> this.perfilUsuario.username;
+      let listaActividadesExistentes = this.apiService.getActividades().subscribe(
+        (listaActividades)=>{
+          let listaPreferencias = this.apiService.getPreferenciasUsuario(nombreUsuario).subscribe(
+            (listaPreferencias)=>{
+        
+              //Agrego a la lista con las actividades existentes
+              listaActividades.forEach(
+                (actividad) => {
+                  const matchingPreferencia = listaPreferencias.find(preferencia => preferencia.nombre === actividad.nombre);
+                  if (matchingPreferencia) {
+                    this.activities.push({name: actividad.nombre, selected:true})
+                  } else{
+                    this.activities.push({name: actividad.nombre, selected:false})
+                  }
+                }
+              )
+            }
+          )
+        }
+      );
+
+    }
+    else {
+        console.log("El usuario no esta logeado")
+      };
+  }
 
   saveActivities() {
     console.log('Actividades seleccionadas:', this.activities.filter(activity => activity.selected));
